0cdc7707b12d8a3271be5e55b34cba1d05b2abec
Kyle McCormick <kdmc@pm.me>
2022-05-04 08:17:21 -0400

    several fixes to firefox profiles
    * remove kyle-plus and kyle-edx
    * remove Crash Reports
    * share user.js and userChrome.css via new _reference folder

---
+++ .mozilla/firefox/_reference/user.js
@@ -0,0 +1,6 @@
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
+
+//user_pref("extensions.activeThemeID", "{074764e0-6dd5-4683-9947-06ee2e2df544}");
