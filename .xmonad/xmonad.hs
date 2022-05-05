d05237bc2996ff85c0f871ec41107c1127f86be5
Kyle McCormick <kdmc@pm.me>
2022-05-05 00:13:08 -0400

    remove unbindings from xmonad.hs; all are explicit now

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 
 import Control.Concurrent ( threadDelay )
 import Data.List ( intercalate )
+import qualified Data.Map as M
 import Numeric ( showHex )
 import System.Directory ( doesFileExist )
 import System.Exit ( exitWith, ExitCode(..) )
@@ -11,13 +12,14 @@ import XMonad
     xmonad, def,
 	keys, borderWidth, normalBorderColor, focusedBorderColor, terminal, modMask,
 	startupHook, manageHook, layoutHook, handleEventHook, logHook,
-	X, XConfig(XConfig),
-	spawn, windows, withFocused, sendMessage, kill,
+	X, XConfig(XConfig), WorkspaceId,
+	spawn, windows, withFocused, sendMessage, kill, refresh,
 	mod1Mask, mod4Mask, noModMask, shiftMask,
     (|||), (.|.), (<+>)
   )
 import XMonad.Actions.SinkAll ( sinkAll )
 import XMonad.Actions.SpawnOn ( spawnOn, manageSpawn )
+import XMonad.Config.Prime  ( Resize(Shrink, Expand) )
 import XMonad.Config.Xfce ( xfceConfig )
 import XMonad.Hooks.EwmhDesktops
   (
@@ -29,15 +31,14 @@ import XMonad.Layout ( Full(Full) )
 import XMonad.Layout.ResizableTile
   ( MirrorResize(MirrorExpand, MirrorShrink), ResizableTall(ResizableTall) )
 import XMonad.Layout.StackTile ( StackTile(StackTile) )
-import XMonad.StackSet ( focusDown, focusUp, swapDown, swapUp )
-import XMonad.Util.CustomKeys ( customKeys )
+import XMonad.StackSet ( focusDown, focusUp, swapDown, swapUp, shift, greedyView)
 import XMonad.Util.Run ( spawnPipe, runProcessWithInput )
 
 import qualified Graphics.X11.Types as X11T
 
 main = do
     xmonad $ xfceConfig
-          { keys = customKeys delKeys insKeys
+          { keys = (\_ -> M.fromList $ map (\(m, k, c) -> ((m, k), c)) keybindings)
           , modMask = global
           , borderWidth = 3
 		  , focusedBorderColor = "#bb00ff"
@@ -49,11 +50,6 @@ main = do
 		  , handleEventHook = docksEventHook <+> ewmhDesktopsEventHook
           , logHook    = ewmhDesktopsLogHook
           }
-    where
-        delKeys XConfig {modMask = modm} = unbindings
-        insKeys XConfig {modMask = modm} = (
-            map (\(m, k, c) -> ((m, k), c)) keybindings
-          )
 
 myLayout = avoidStruts $ tiled ||| goldenStack ||| Full
         where
@@ -67,41 +63,26 @@ onStart :: X ()
 onStart =
     ewmhDesktopsStartup >>                     -- Init EWMH stuff.
 	spawn "sleep 2; xfce4-panel --restart" >>  -- Restart panel show XMonad respects it.
-	spawn "invertscroll" >>                    -- Ensure natural scrolling direction.
+	spawn "/home/kyle/invertscroll" >>         -- Ensure natural scrolling direction.
 	spawn "xset s 300 5" >>                    -- Lock screen after 300s; poll every 5s.
 	spawn "xss-lock --notifier=/usr/local/libexec/xsecurelock/dimmer --transfer-sleep-lock -- xsecurelock"
 
 spawnWks :: Int -> String -> X ()
 spawnWks wksId cmd = spawnOn (show wksId) cmd
 
-quitFocused :: X ()
-quitFocused = withFocused  (\w -> do
-        let idStr = "0x" ++ showHex w ""
-        let cmd = "wmctrl -i -c " ++ idStr
-        spawn cmd
-    )
-
-isRunning :: String -> X Bool
-isRunning s = do
-    output <- runProcessWithInput "pgrep" [s] ""
-    return $ not $ null output
-
 -- Modifier keys
 noMod          = noModMask
-shift          = shiftMask
+shft           = shiftMask
 alt            = mod1Mask
-altShift       = alt .|. shift
+altShift       = alt .|. shft
 super          = mod4Mask
-superShift     = super .|. shift
+superShift     = super .|. shft
 global         = super
-globalShift    = global .|. shift
+globalShift    = global .|. shft
 globalAlt      = global .|. alt
 
--- Key un-bindings
-unbindings =
-    [ (alt, X11T.xK_VoidSymbol)  -- Don't show menu on Alt
-	,  (global, X11T.xK_d)
-    ]
+workspaces :: [WorkspaceId]
+workspaces = map show [1 .. 9 :: Int]
 
 -- Keybindings
 keybindings =
@@ -111,13 +92,15 @@ keybindings =
     , (globalShift,   X11T.xK_k,         windows swapUp)
 --    , (global,        X11T.xK_h,         sendMessage Shrink)
 --    , (global,        X11T.xK_l,         sendMessage Expand)
+    , (global,        X11T.xK_h,         sendMessage Shrink)
+    , (global,        X11T.xK_l,         sendMessage Expand)
     , (globalShift,   X11T.xK_h,         sendMessage MirrorExpand)
     , (globalShift,   X11T.xK_l,         sendMessage MirrorShrink)
     , (global,        X11T.xK_d,         kill)
     , (global,        X11T.xK_space,     spawn cmdMenu)
-    , (global,        X11T.xK_Return,    spawn cmdTerminalDropDown)
-    , (globalShift,   X11T.xK_Return,    spawn cmdTerminal)
-    , (global,        X11T.xK_backslash, sinkAll)
+--    , (global,        X11T.xK_Return,    spawn cmdTerminalDropDown)
+    , (global,        X11T.xK_i,         spawn cmdTerminal)
+    , (global,        X11T.xK_Escape,    sinkAll >> refresh)
 --    , (globalShift,   X11T.xK_f,         spawn cmdFirefox)
 --    , (globalShift,   X11T.xK_w,         spawn cmdChromium)
 --    , (globalShift,   X11T.xK_e,         spawn cmdTextEditor)
@@ -129,7 +112,12 @@ keybindings =
 --    , (globalShift,   X11T.xK_s,         spawnMulti [cmdWMQuit, cmdShutdown])
 --    , (global,        X11T.xK_0,         spawn cmdOneMonitor)
 --    , (globalShift,   X11T.xK_0,         spawn cmdTwoMonitorsVertical)
-    ]
+    ] ++
+    -- mod-[1..9] %! Switch to workspace N
+    -- mod-shift-[1..9] %! Move client to workspace N
+    [(global .|. m, k, windows $ f i)
+        | (i, k) <- zip workspaces [X11T.xK_1 .. X11T.xK_9]
+        , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
 
 cmdTextEditor             = "nvim" -- cmdSublime True
 cmdSublime new            = "subl -w" ++ if new then " -n" else ""
@@ -137,16 +125,17 @@ cmdTerminal               = "xfce4-terminal"
 cmdTerminalDropDown       = "xfce4-terminal --drop-down"
 cmdMenu                   = "xfce4-popup-whiskermenu"
 cmdChromium               = "chromium-browser --new-window"
-cmdFirefox                = "firefox"
+cmdFirefox                = "firefox -p $KI_USER_PROFILE"
+cmdFirefoxProfiles        = "firefox -p"
 cmdLock                   = "xflock4" --"xset s activate"
 cmdSuspend                = "systemctl suspend"
 cmdSessionDialog          = "xfce4-session-logout"
 cmdWMQuit                 = "wmquit"
-cmdExit                   = "killall -SIGINT xmonad-x86_64-linux"
-cmdShutdown               = "shutdown -P now"
-cmdReboot                 = "shutdown -r now"
-cmdOneMonitor             = "mon L"
-cmdTwoMonitorsVertical    = "mon a"
+--cmdExit                   = "killall -SIGINT xmonad-x86_64-linux"
+--cmdShutdown               = "shutdown -P now"
+--cmdReboot                 = "shutdown -r now"
+--cmdOneMonitor             = "mon L"
+--cmdTwoMonitorsVertical    = "mon a"
 
 spawnMulti cmds = spawn $ intercalate " && " cmds
 
